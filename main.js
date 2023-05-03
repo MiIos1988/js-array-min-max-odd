4908c25fa3623fed8508a4b059fc4f46c360f3de
Milos1988 <vojvoda1988@gmail.com>
2023-05-03 08:10:17 +0200

    Solution of the task.

---
+++ main.js
@@ -0,0 +1,13 @@
+function calculateArray(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    const oddCount = arr.filter(num => num % 2 !== 0).length;
+
+    console.log(`Minimum: ${min}`);
+    console.log(`Maximum: ${max}`);
+    console.log(`Number of odd numbers: ${oddCount}`);
+
+}
+
+const arr = [1, 2, 3, 4, 5];
+calculateArray(arr);
